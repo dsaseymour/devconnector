dc61e467d9fdf55bfe3dcf3df34b6e75ea9f7b78
Danny <dannyseymour1990@gmail.com>
2018-11-27 20:37:38 -0500

    before executing  reducer error

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,31 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
 import "./App.css";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+//Check whether the user is logged in or not on each page request
+//look for token in local storage if the token exists set the auth token header
+//decode the token and get data
+/*
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+}
+
+ */
 
 class App extends Component {
   render() {
